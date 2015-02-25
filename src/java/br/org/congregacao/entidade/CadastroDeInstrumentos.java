88298b53c8385cb7ed273d2a6a892b06b321a89e
vagneracacio <vagneracaciodasilva@yahoo.com.br>
2015-02-25 19:35:26 -0300

    create structure init my project config

---
+++ src/java/br/org/congregacao/entidade/CadastroDeInstrumentos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.congregacao.entidade;
+
+/**
+ *
+ * @author Cliente PC
+ */
+public class CadastroDeInstrumentos {
+    
+    private long id;
+    private String nomeIntrumento;
+    private String categoriaIntrumento;
+        
+}
---
+++ src/java/br/org/congregacao/entidade/CadastroDeMinisterio.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.congregacao.entidade;
+
+/**
+ *
+ * @author Cliente PC
+ */
+public class CadastroDeMinisterio {
+    private long id;
+    private String nome;
+    private String cargo;
+    
+    
+}
